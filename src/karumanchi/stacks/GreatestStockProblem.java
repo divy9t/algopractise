4a67f1e5550b5fe9cc17848964ca321eab4ea9b5
Divyansh Tripathi <divy9t12@icloud.com>
2024-03-19 21:19:20 +0530

    [ADD] - Stacks - greatest stock problem solution using stacks

--- src/karumanchi/stacks/GreatestStockProblem.java
+++ src/karumanchi/stacks/GreatestStockProblem.java
@@ -7,8 +7,8 @@ public class GreatestStockProblem {
 
     public static void main(String[] args) {
 
-        int[] s = findSpans(new int[]{6,3,4,5,2}, 5);
-        System.out.println(Arrays.toString(s));
+        System.out.println(Arrays.toString(findSpans(new int[]{6, 3, 4, 5, 2}, 5)));
+        System.out.println(Arrays.toString(findSpanUsingStack(new int[]{6, 3, 4, 5, 2}, 5)));
     }
 
     private static int[] findSpans(int[] A, int num) {
@@ -23,10 +23,46 @@ public class GreatestStockProblem {
 
             // we used the while loop here because we wanted to keep adding the value till
             // the condition doesn't break
-            while (j <= i && A[i] > A[i-j])
+            while (j <= i && A[i] > A[i - j])
                 j = j + 1;
 
             result[i] = j;
+        }
+
+        return result;
+
+    }
+
+    private static int[] findSpanUsingStack(int[] A, int num) {
+
+        int[] result = new int[A.length];
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        result[0] = 1;
+
+        for (int i=0; i < num; i++) {
+            /*
+            if the stack is not empty and the current element in the stack is greater than the
+            top element of the stack, then we will pop the stack ensuring that only the greater element
+            stays in the stack
+             */
+            while (!stack.isEmpty() && A[stack.peek()] <= A[i] ) {
+                stack.pop();
+            }
+
+
+            /*
+            if stack is empty then it means the current element is the largest element,
+            if not then it is greater than the ith element
+             */
+            result[i] = stack.isEmpty() ? i + 1: i - stack.peek();
+
+            /*
+            push the current element in stack
+             */
+            stack.push(i);
+
         }
 
         return result;
